e4744544d4194fdf8f71e584440e21c6fbb7b02a
kauezero <kauezero1@gmail.com>
2025-05-07 19:53:07 -0300

    CSS changes and pagination

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,14 +1,37 @@
 const express = require('express');
 const router = express.Router();
+const Post = require('../models/Post');
 
-// Routes
-router.get('', (req, res) => {
-  const locals = {
-    title: "NodeJS Blog",
-    description: "Simple blog created with NodeJS, Express and MongoDB."
+// GET - home
+router.get('', async (req, res) => {
+  try {
+    const locals = {
+      title: "NodeJS Blog",
+      description: "Simple blog created with NodeJS, Express and MongoDB."
+    }
+
+    let perPage = 10;
+    let page = req.query.page || 1;
+
+    const data = await Post.aggregate([{ $sort: { createdAt: -1 } }])
+    .skip(perPage * page - perPage)
+    .limit(perPage)
+    .exec();
+
+    const count = await Post.countDocuments();
+    const nextPage = parseInt(page) + 1;
+    const hasNextPage = nextPage <= Math.ceil(count / perPage);
+
+    res.render('index', { 
+      locals,
+      data,
+      current: page,
+      nextPage: hasNextPage ? nextPage : null
+    });
+
+  } catch (error) {
+    console.log(error);
   }
-
-  res.render('index', { locals });
 });
 
 router.get('/about', (req, res) => {
