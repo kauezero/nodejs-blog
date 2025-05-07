e1a036878647c22354a3851b6a8ded12c0b293bb
kauezero <kauezero1@gmail.com>
2025-05-07 20:33:37 -0300

    Search functionality

--- server/routes/main.js
+++ server/routes/main.js
@@ -51,7 +51,35 @@ router.get('/post/:id', async (req, res) => {
   } catch (error) {
     console.log(error);
   }
-})
+});
+
+// POST - post - searchTerm
+router.post('/search', async (req, res) => {
+  try {
+    const locals = {
+      title: "Search",
+      description: "Simple blog created with NodeJS, Express and MongoDB."
+    }
+
+    let searchTerm = req.body.searchTerm;
+    const searchNoSpecialChar = searchTerm.replace(/[^a-zA-Z0-9 ]/g, "");
+
+
+    const data = await Post.find({
+      $or: [
+        { title: { $regex: new RegExp(searchNoSpecialChar, 'i') }},
+        { body: { $regex: new RegExp(searchNoSpecialChar, 'i') }}
+      ]
+    });
+
+    res.render('search', {
+      data,
+      locals
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 router.get('/about', (req, res) => {
   res.render('about');
