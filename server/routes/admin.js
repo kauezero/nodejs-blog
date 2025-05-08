b3944587a601ee20936784316d898dd43c4c6bf8
kauezero <kauezero1@gmail.com>
2025-05-07 21:14:26 -0300

    Admin login and register functionality

---
+++ server/routes/admin.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const router = express.Router();
+const Post = require('../models/Post');
+const User = require('../models/User');
+const { render } = require('ejs');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const jwtSecret = process.env.JWT_SECRET;
+
+const adminLayout = '../views/layouts/admin'
+
+// GET - admin - login
+router.get('/admin', (req,res) => {
+    try {
+        const locals = {
+            title: "NodeJS Blog",
+            description: "Simple blog created with NodeJS, Express and MongoDB."
+        }
+
+        res.render('admin/index', { locals, layout: adminLayout });
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+// POST - admin - login
+router.post('/admin', async (req, res) => {
+    try {
+        const {username, password} = req.body;
+
+         const user = await User.findOne({ username });
+         if (!user) {
+            return res.status(401).json({ message: 'Invalid credentials' });
+         }
+
+         const isPasswordValid = await bcrypt.compare(password, user.password);
+         if (!isPasswordValid) {
+            return res.status(401).json({ message: 'Invalid credentials' });
+         }
+
+         const token = jwt.sign({ userId: user._id }, jwtSecret);
+         res.cookie('token', token, { httpOnly: true });
+
+        res.redirect('/dashboard');
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+// POST - admin - register
+router.post('/register', async (req, res) => {
+    try {
+        const {username, password} = req.body;
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        try {
+            const user = await User.create({ username, password:hashedPassword });
+            res.status(201).json({ message: 'User created', user });
+        } catch (error) {
+            if (error.code === 11000) {
+                res.status(409).json({ message: 'User already in use' })
+            }
+            res.status(500).json({ message: 'Internal server error' });
+        }
+
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+// GET - admin - dashboard
+router.get('/dashboard', async (req, res) => {
+    res.render('admin/dashboard');
+})
+
+module.exports = router;
